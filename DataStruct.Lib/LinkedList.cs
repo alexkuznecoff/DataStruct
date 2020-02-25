b1e4443f2963518c0042cebd64d839848434839e
a.kuznecoff82@gmail.com <a.kuznecoff82@gmail.com>
2020-02-25 20:24:32 +0200

    LinkedListCommit

--- DataStruct.Lib/LinkedList.cs
+++ DataStruct.Lib/LinkedList.cs
@@ -6,7 +6,150 @@ using System.Threading.Tasks;
 
 namespace DataStruct.Lib
 {
-    class LinkedList
+    public class LinkedList
     {
+        public int Count { get; set; }
+        public Node First { get; set; }
+        public Node Last { get; set; }
+
+        public void Add(object data)
+        {
+            Node node = new Node(data);
+            if (First == null)
+            {
+                First = node;
+                Last = node;
+            }
+            else
+            {
+                Last.Next = node;
+                Last = node;
+            }
+            Count++;
+        }
+
+        public void AddFirst(object data)
+        {
+            Node node = new Node(data);
+
+            if (First == null)
+            {
+                First = node;
+                Last = node;
+            }
+            node.Next = First;
+            First = node;
+            Count++;
+        }
+
+        public void Insert(Node node, object data)
+        {
+            var currentNode = First;
+
+            do
+            {
+                if (currentNode == node
+                    && data is Node newNode)
+                {
+                    newNode.Next = currentNode.Next;
+                    currentNode.Next = newNode.Next;
+                    Count++;
+                    break;
+                }
+                currentNode = currentNode.Next;
+
+            } while (currentNode != null);
+        }
+
+        public void Clear()
+        {
+            Node current = First;
+            Count = 0;
+            do
+            {
+                if (current.Next != null)
+                {
+                    Node tempCurrent = current.Next;
+                    current.Data = null;
+                    current = tempCurrent;
+                    
+                }
+
+            }
+            while (current != null);
+            First = null;
+            Last = null;
+
+        }
+
+        public bool Contains(object data)
+        {
+            Node current = First;
+
+            while (current != null)
+            {
+                if (current.Data == data)
+                    return true;
+
+                current = current.Next;
+            }
+            return false;
+        }
+
+        public object[] ToArray()
+        {
+            object[] array = new object[Count];
+            Node current = First;
+
+            for (int i = 0; i < Count; i++)
+            {
+                array[i] = current.Data;
+                current = current.Next;
+            }
+            return array;
+        }
+
+        public bool Remove(object data)
+        {
+            Node current = First;
+            Node previous = null;
+
+            while(current !=null)
+            {
+                if(current.Data == data)
+                {
+                    if(previous != null)
+                    {
+                        previous.Next = current.Next;
+
+                        if (current.Next == null)
+                            Last = previous;
+                    }
+                    else
+                    {
+                        First = First.Next;
+
+                        if (First == null)
+                            Last = null;
+                    }
+                    Count--;
+                    return true;
+                }
+                previous = current;
+                current = current.Next;
+            }
+            return false;
+        }
+    }
+
+    public class Node
+    {
+        public Node Next { get; set; }
+        public object Data { get; set; }
+
+        public Node(object data)
+        {
+            Data = data;
+        }
     }
 }
